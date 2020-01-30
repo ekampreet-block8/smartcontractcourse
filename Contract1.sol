b872da50c77d4c48703df61bbd45d6320afd901a
Ekampreet Singh <ekampreet.singh@block8.com>
2020-01-30 12:32:13 +0530

    assigned value directly instead of constructor

--- Contract1.sol
+++ Contract1.sol
@@ -1,11 +1,7 @@
 pragma solidity ^0.5.1;
 
 contract Contract1 {
-    string public value;
-
-    constructor() public {
-        value = "Hello World";
-    }
+    string public value = "Hello World";
 
     function set(string memory _value) public {
         value = _value;
