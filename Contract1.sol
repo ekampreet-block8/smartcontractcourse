0e341bac9accc27e90eb1282a8c99decf7e503a0
Ekampreet Singh <ekampreet.singh@block8.com>
2020-01-30 12:30:24 +0530

    made value var public removed getter

--- Contract1.sol
+++ Contract1.sol
@@ -1,14 +1,10 @@
 pragma solidity ^0.5.1;
 
 contract Contract1 {
-    string value;
+    string public value;
 
     constructor() public {
         value = "Hello World";
-    }
-
-    function get() public view returns (string memory) {
-        return value;
     }
 
     function set(string memory _value) public {
