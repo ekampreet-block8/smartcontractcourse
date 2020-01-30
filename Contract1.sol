f4258a054550cfd3ed233fb8fcc1b6a79fcd7b47
Ekampreet Singh <ekampreet.singh@block8.com>
2020-01-30 12:46:51 +0530

    enum

--- Contract1.sol
+++ Contract1.sol
@@ -1,10 +1,18 @@
 pragma solidity ^0.5.1;
 
 contract Contract1 {
-    string public stringValue = "Hello World";
-    bool public myBool = true;
-    int256 public myInt = -1;
-    uint256 public myUint = 1;
-    uint8 public myUint8 = 8;
-    uint256 public myUint256 = 9999;
+    enum State {Waiting, Ready, Active}
+    State public state;
+
+    constructor() public {
+        state = State.Waiting;
+    }
+
+    function activate() public {
+        state = State.Active;
+    }
+
+    function isActive() public view returns (bool) {
+        return state == State.Active;
+    }
 }
