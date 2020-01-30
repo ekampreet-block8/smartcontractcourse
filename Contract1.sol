3e50790ab46c7b5ea273e62765c380f863fbdda9
Ekampreet Singh <ekampreet.singh@block8.com>
2020-01-30 12:39:21 +0530

    basic data types

--- Contract1.sol
+++ Contract1.sol
@@ -1,5 +1,10 @@
 pragma solidity ^0.5.1;
 
 contract Contract1 {
-    string public constant value = "Hello World";
+    string public stringValue = "Hello World";
+    bool public myBool = true;
+    int256 public myInt = -1;
+    uint256 public myUint = 1;
+    uint8 public myUint8 = 8;
+    uint256 public myUint256 = 9999;
 }
