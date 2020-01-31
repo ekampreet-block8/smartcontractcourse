c4778d37fb477a6078ee5a44f00313dddd76c2b8
Ekampreet Singh <ekampreet.singh@block8.com>
2020-01-31 15:05:56 +0530

    mint call super

--- MultiContract.sol
+++ MultiContract.sol
@@ -15,10 +15,19 @@ contract ERC20Token {
 
 contract MyToken is ERC20Token {
     string public symbol;
+    address[] public owners;
+    uint256 ownerCount;
+
     constructor(string memory _name, string memory _symbol)
         public
         ERC20Token(_name)
     {
         symbol = _symbol;
+    }
+
+    function mint() public {
+        super.mint();
+        ownerCount++;
+        owners.push(msg.sender);
     }
 }
