e3f0065f2b98239914b853eae52e93bdfd9397d5
Ekampreet Singh <ekampreet.singh@block8.com>
2020-01-31 14:52:29 +0530

    inheritance name

--- MultiContract.sol
+++ MultiContract.sol
@@ -4,26 +4,15 @@ contract ERC20Token {
     string public name;
     mapping(address => uint256) public balances;
 
+    constructor(string memory _name) public {
+        name = _name;
+    }
+
     function mint() public {
         balances[tx.origin]++;
     }
 }
 
-contract MyContract {
-    address payable wallet;
-    address public token;
-
-    constructor(address payable _wallet, address _token) public {
-        wallet = _wallet;
-        token = _token;
-    }
-
-    function() external payable {
-        buyToken();
-    }
-
-    function buyToken() public payable {
-        ERC20Token(address(token)).mint();
-        wallet.transfer(msg.value);
-    }
+contract MyToken is ERC20Token {
+    string public name = "My Token";
 }
